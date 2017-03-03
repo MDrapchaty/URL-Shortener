2978a3d55f294094fe49a261ca60ca411cd853f9
Matt Drapchaty <Drapchaty@comcast.net>
2017-03-02 19:38:30 -0700

    Unable to connect mysql DB

---
+++ models/db.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS, {
+    host: process.env.DB_HOST,    // Set the host.
+    dialect: process.env.DB_SCHEMA,    // Set the dialect.
+    port: process.env.DB_PORT,    // Set the port.
+    // Set the max and min connections and maximum idle time.
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+    },
+    
+    logging: false,
+  });
+
+const url = sequelize.define('urls',{
+	Url: {
+		type: Sequelize.STRING,
+	},
+	shortUrl: {
+		type: Sequelize.STRING,
+	},
+});
+
+//sequelize.authenticate();
+
+sequelize.sync();
+
+exports.sequelize = sequelize;
+exports.url = url;
+
+
+
